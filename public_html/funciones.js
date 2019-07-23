be4db0b564e53a0e0fdb09f52f04d60f025ce41f
Denilson de León <denii.grifon@gmail.com>
2019-07-23 12:56:48 -0600

    Version_1.1
    Separa los distintos tipos de cadenas y agrega color rojo si es "ERROR"

--- public_html/funciones.js
+++ public_html/funciones.js
@@ -5,37 +5,67 @@ var iniciaConAZ = document.getElementById("iniciaConAZ");
 var tieneLetras = document.getElementById("tieneLetras");
 var tieneNumeros = document.getElementById("tieneNumeros");
 var tieneSimbolos = document.getElementById("tieneSimbolos");
-var tipo = "error";
+var tipo = "ERROR";
 var contieneLetras = 0, contieneSimbolos = 0, contieneNumeros = 0, iniciaLetras = false;
 function reconocer(){    
     inicializarVar();
     if(texto == ""){
         alert("rellena la casilla"); 
     }
-    else{
+    else{        
+        analizarCaracteres();
+        identificarTipo();
+        colocarDatosHistorial();
         
-        for(var i = 0; i < texto.length; i++){            
-            if(texto.charCodeAt(i) > 64 && texto.charCodeAt(i) < 91 || texto.charCodeAt(i) > 96 && texto.charCodeAt(i) < 123){
-                if(i == 0)
-                    iniciaLetras = true;
-                contieneLetras++;
-            }
-            else if(texto.charCodeAt(i) > 47 && texto.charCodeAt(i) < 58){
-                contieneNumeros++;
-            }
-            else
-                contieneSimbolos++;
-        }
-        palabra.innerHTML = palabra.innerHTML + texto + "<br></br>";
-        clasificacion.innerHTML = clasificacion.innerHTML + tipo + "<br></br>";
-        iniciaConAZ.innerHTML = iniciaConAZ.innerHTML + iniciaLetras + "<br></br>";
-        tieneLetras.innerHTML = tieneLetras.innerHTML + contieneLetras + "<br></br>";
-        tieneNumeros.innerHTML = tieneNumeros.innerHTML + contieneNumeros + "<br></br>";
-        tieneSimbolos.innerHTML = tieneSimbolos.innerHTML + contieneSimbolos +"<br></br>";
-        texto="";
     }      
 }
 function inicializarVar(){
     texto = document.getElementById("texto").value.toString();
     contieneLetras = 0, contieneSimbolos = 0, contieneNumeros = 0, iniciaLetras = false;
+    tipo = "ERROR";
+}
+function analizarCaracteres(){
+        for(var i = 0; i < texto.length; i++){            
+            if(texto.charCodeAt(i) > 64 && texto.charCodeAt(i) < 91 || texto.charCodeAt(i) > 96 && texto.charCodeAt(i) < 123){
+                if(i == 0){
+                    iniciaLetras = true;
+                }
+                contieneLetras++;
+            }
+            else if(texto.charCodeAt(i) > 47 && texto.charCodeAt(i) < 58){
+                contieneNumeros++;
+            }
+            else{
+                contieneSimbolos++;
+            }
+        }
+}
+function identificarTipo(){
+    if(iniciaLetras){
+        if(contieneSimbolos == 0)
+            tipo = "IDENTIFICADOR";        
+    }
+    else{
+        if(contieneNumeros > 0 && contieneLetras == 0 && contieneSimbolos == 0)
+            tipo = "NUMERO";
+        else if(contieneNumeros == 0 && contieneLetras == 0 && contieneSimbolos > 0) 
+            tipo = "SIMBOLOS";
+    }
+}
+function colocarDatosHistorial(){
+        var tipoDeP = "<p>";
+        if(tipo == "ERROR")
+            tipoDeP = "<p class='colorRojo'>";
+        palabra.innerHTML = palabra.innerHTML + tipoDeP +texto + "</p>";        
+        clasificacion.innerHTML = clasificacion.innerHTML + tipoDeP + tipo + "</p>";        
+        iniciaConAZ.innerHTML = iniciaConAZ.innerHTML + tipoDeP + imprimirSiNo(iniciaLetras) + iniciaLetras + ")</p>";
+        tieneLetras.innerHTML = tieneLetras.innerHTML + tipoDeP + imprimirSiNo(contieneLetras) + contieneLetras + ")</p>";
+        tieneNumeros.innerHTML = tieneNumeros.innerHTML + tipoDeP + imprimirSiNo(contieneNumeros) + contieneNumeros + ")</p>";
+        tieneSimbolos.innerHTML = tieneSimbolos.innerHTML + tipoDeP + imprimirSiNo(contieneSimbolos) + contieneSimbolos + ")</p>";        
+}
+function imprimirSiNo(a){       
+        if(a || a > 0)
+            return "SI_(";
+        else
+            return "NO_(";
 }
